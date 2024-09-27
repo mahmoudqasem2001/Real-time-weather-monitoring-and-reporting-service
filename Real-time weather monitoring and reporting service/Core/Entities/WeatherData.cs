8ecd4839ae88d66e40e6dac9c0d46bfba60fa09e
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-09-27 22:40:32 +0300

    Implement Entities and Interfaces in Core Layer

---
+++ Real-time weather monitoring and reporting service/Core/Entities/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_time_weather_monitoring_and_reporting_service.Core.Entities
+{
+    public class WeatherData
+    {
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+    }
+}
---
+++ Real-time weather monitoring and reporting service/Core/Interfaces/IWeatherBot.cs
@@ -0,0 +1,14 @@
+﻿using Real_time_weather_monitoring_and_reporting_service.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_time_weather_monitoring_and_reporting_service.Core.Interfaces
+{
+    public interface IWeatherBot
+    {
+        void Activate(WeatherData data);
+    }
+}
