5743256fdc60152e90169633d43b54d38f1dcaea
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-09-27 22:41:16 +0300

    Implement Configuration and Input Output in Infrastructure layer

---
+++ Real-time weather monitoring and reporting service/Infrastructure/Configuration/BotConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_time_weather_monitoring_and_reporting_service.Infrastructure.Configuration
+{
+    public class BotConfig
+    {
+        public bool Enabled { get; set; }
+        public double Threshold { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ConfigRoot
+    {
+        public BotConfig RainBot { get; set; }
+        public BotConfig SunBot { get; set; }
+        public BotConfig SnowBot { get; set; }
+    }
+}
---
+++ Real-time weather monitoring and reporting service/Infrastructure/IO/ConfigurationReader.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Real_time_weather_monitoring_and_reporting_service.Infrastructure.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_time_weather_monitoring_and_reporting_service.Infrastructure.IO
+{
+    public class ConfigurationReader
+    {
+        public static ConfigRoot LoadConfig(string filePath)
+        {
+            string configJson = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<ConfigRoot>(configJson);
+        }
+    }
+}
