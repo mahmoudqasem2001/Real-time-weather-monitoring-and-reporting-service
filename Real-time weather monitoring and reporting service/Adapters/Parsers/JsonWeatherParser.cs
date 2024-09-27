572a98151c184c0b25662cd60fd6b670750172aa
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-09-27 22:39:14 +0300

    Implement Parsers

---
+++ Real-time weather monitoring and reporting service/Adapters/Parsers/JsonWeatherParser.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Real_time_weather_monitoring_and_reporting_service.Core.Entities;
+using Real_time_weather_monitoring_and_reporting_service.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_time_weather_monitoring_and_reporting_service.Adapters.Parsers
+{
+    public class JsonWeatherParser : IWeatherParser
+    {
+        public WeatherData Parse(string input)
+        {
+            return JsonConvert.DeserializeObject<WeatherData>(input);
+        }
+    }
+}
---
+++ Real-time weather monitoring and reporting service/Adapters/Parsers/XmlWeatherParser.cs
@@ -0,0 +1,25 @@
+﻿using Real_time_weather_monitoring_and_reporting_service.Core.Entities;
+using Real_time_weather_monitoring_and_reporting_service.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Real_time_weather_monitoring_and_reporting_service.Adapters.Parsers
+{
+    public class XmlWeatherParser : IWeatherParser
+    {
+        public WeatherData Parse(string input)
+        {
+            XElement xElement = XElement.Parse(input);
+            return new WeatherData
+            {
+                Location = xElement.Element("Location")?.Value,
+                Temperature = double.Parse(xElement.Element("Temperature")?.Value),
+                Humidity = double.Parse(xElement.Element("Humidity")?.Value)
+            };
+        }
+    }
+}
