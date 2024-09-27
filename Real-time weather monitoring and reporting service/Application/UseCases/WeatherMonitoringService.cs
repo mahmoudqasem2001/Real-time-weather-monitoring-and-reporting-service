8895598e1d20dcf7dad70c9d3153fdda69392d4a
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-09-27 22:39:54 +0300

    Implement UseCases

---
+++ Real-time weather monitoring and reporting service/Application/UseCases/WeatherMonitoringService.cs
@@ -0,0 +1,28 @@
+﻿using Real_time_weather_monitoring_and_reporting_service.Core.Entities;
+using Real_time_weather_monitoring_and_reporting_service.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_time_weather_monitoring_and_reporting_service.Application.UseCases
+{
+    public class WeatherMonitoringService
+    {
+        private readonly List<IWeatherBot> _bots = new List<IWeatherBot>();
+
+        public void RegisterBot(IWeatherBot bot)
+        {
+            _bots.Add(bot);
+        }
+
+        public void NotifyBots(WeatherData data)
+        {
+            foreach (var bot in _bots)
+            {
+                bot.Activate(data);
+            }
+        }
+    }
+}
